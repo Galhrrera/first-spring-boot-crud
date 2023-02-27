70d0438a9e14a633d9f20a209837b83e807c9f79
Juan Esteban Galeano Herrera <jegaleanoherrera@gmail.com>
2023-02-27 14:48:38 -0500

    Se crearon los modelos

---
+++ src/main/java/com/firstcrud/model/Estado.java
@@ -0,0 +1,48 @@
+package com.firstcrud.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "estado")
+public class Estado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // Para que genere el ID automáticamente
+    private int id;
+    private String nombre;
+    private Pais pais;
+
+    public Estado(String nombre, Pais pais) {
+        this.nombre = nombre;
+        this.pais = pais;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
+
+}
