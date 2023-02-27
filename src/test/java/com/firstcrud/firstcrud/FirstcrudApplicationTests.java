988268d54294a07d7f57b4949be91bed6a46ff3d
Juan Esteban Galeano Herrera <jegaleanoherrera@gmail.com>
2023-02-27 13:00:07 -0500

    Estructura del proyecto en sprint boot con maven

---
+++ src/test/java/com/firstcrud/firstcrud/FirstcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstcrud.firstcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
