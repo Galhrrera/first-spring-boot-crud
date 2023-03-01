23637e851de4b06f94b7136f4f0bc31d0e8b8118
Juan Esteban Galeano Herrera <jegaleanoherrera@gmail.com>
2023-03-01 14:26:22 -0500

    Se corrigieron los controladores

--- src/main/java/com/firstcrud/demo/rest/PaisREST.java
+++ src/main/java/com/firstcrud/demo/rest/PaisREST.java
@@ -1,5 +1,26 @@
 package com.firstcrud.demo.rest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.firstcrud.demo.model.Pais;
+import com.firstcrud.demo.service.PaisService;
+
+@RestController
+@RequestMapping("/pais/")
 public class PaisREST {
-    
+
+    @Autowired
+    private PaisService paisService;
+
+    @GetMapping
+    private ResponseEntity<List<Pais>> getAllPaises() {
+        return ResponseEntity.ok(paisService.findAll());
+    }
+
 }
